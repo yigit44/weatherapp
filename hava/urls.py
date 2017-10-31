af535e1d20b8219ceed054d594af562e2b408dc4
yigit44 <yigit.balalan@gmail.com>
2017-10-31 17:07:02 +0300

    lastcommit

--- hava/urls.py
+++ hava/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
 from hava.views import WeatherSituationView
+from . import views
 
 urlpatterns = [
 	url(r'^current-weather/$', WeatherSituationView.as_view(), name='SendCurrentWeather'),
--- hava/views.py
+++ hava/views.py
@@ -8,9 +8,11 @@ from rest_framework.generics import GenericAPIView
 
 # Create your views here.
 class WeatherSituationView(GenericAPIView):
-	def post(self):
-		city_name = self.request.data.get('city')
+	serializer_class = ""
+	def post(self,request):
+		city_name = request.data.get('city')
 		weather_url = settings.OPEN_WEATHER_API
+	
 		params = {
 			'q': city_name,
   			'appid': '0fa71fc77f705bab55a6027f6e1e5995'
@@ -18,4 +20,5 @@ class WeatherSituationView(GenericAPIView):
 		r = requests.get(weather_url, params=params)
 		json_object = r.json()
 		temp_k = float(json_object['main']['temp'])
-		return temp_k
+
+		return str(temp_k)
