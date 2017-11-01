afc1080ccfe54434b4c4c734db44434ddfa9d6f2
yigit44 <yigit.balalan@gmail.com>
2017-11-02 00:20:50 +0300

    duzeltmeler yapildi

--- hava/views.py
+++ hava/views.py
@@ -1,6 +1,8 @@
 import requests
+import json
 from django.shortcuts import render
 from django.conf import settings
+from django.http import HttpResponse
 
 from rest_framework.generics import GenericAPIView
 
@@ -9,6 +11,7 @@ from rest_framework.generics import GenericAPIView
 # Create your views here.
 class WeatherSituationView(GenericAPIView):
 	serializer_class = ""
+	
 	def post(self,request):
 		city_name = request.data.get('city')
 		weather_url = settings.OPEN_WEATHER_API
@@ -18,7 +21,9 @@ class WeatherSituationView(GenericAPIView):
   			'appid': '0fa71fc77f705bab55a6027f6e1e5995'
 		}
 		r = requests.get(weather_url, params=params)
-		json_object = r.json()
-		temp_k = float(json_object['main']['temp'])
+		json_data = json.loads(r.text)
+		json_data1 = json_data['main']['temp']
+		return HttpResponse( json.dumps( json_data1))
+		
 
-		return str(temp_k)
+		
